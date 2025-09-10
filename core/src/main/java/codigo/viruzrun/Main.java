dfd949c01bb4e0afede21cfb900e51bec808d4e0
PazMauro <pazmauro2006@gmail.com>
2025-09-10 15:14:30 -0300

    primer commit

--- core/src/main/java/codigo/viruzrun/Main.java
+++ core/src/main/java/codigo/viruzrun/Main.java
@@ -22,7 +22,7 @@ public class Main extends ApplicationAdapter {
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 546, 324);
+        batch.draw(image, 2343, 32432);
         batch.end();
     }
 
