3b5e151a1ec65578bdb5e38968bcc6070865f9e2
PazMauro <132943783+PazMauro@users.noreply.github.com>
2026-01-27 18:05:56 -0300

    Add files via upload

--- lwjgl3/src/main/java/codigo/viruzrun/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/codigo/viruzrun/lwjgl3/Lwjgl3Launcher.java
@@ -4,34 +4,27 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import codigo.viruzrun.Main;
 
-/** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
+
+    public static final int ANCHO = 800;
+    public static final int ALTO = 480;
+
     public static void main(String[] args) {
-        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
-        createApplication();
-    }
 
-    private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
-    }
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
-    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
-        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("ViruzRun");
-        //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
-        //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
-        //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
-        //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
-        //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
-        //// useful for testing performance, but can also be very stressful to some hardware.
-        //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+        config.setTitle("Viruz Run");
 
-        configuration.setWindowedMode(640, 480);
-        //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-        return configuration;
+        // 🔒 RESOLUCIÓN FIJA
+        config.setWindowedMode(ANCHO, ALTO);
+        config.setResizable(false);
+
+        // 🎯 FPS ESTABLE
+        config.setForegroundFPS(60);
+
+        // 🧼 VSYNC
+        config.useVsync(true);
+
+        new Lwjgl3Application(new Main(), config);
     }
 }
